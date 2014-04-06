7d2568eee9e0b1229fb72b5c9c08a85d67b8fa9a
c9s <yoanlin93@gmail.com>
2014-04-06 15:15:11 +0800

    Rename comment to remark

--- Model/Mixin/RemarkSchema.php
+++ Model/Mixin/RemarkSchema.php
@@ -2,12 +2,12 @@
 namespace CommonBundle\Model\Mixin;
 use LazyRecord\Schema\MixinSchemaDeclare;
 
-class CommentSchema extends MixinSchemaDeclare
+class RemarkSchema extends MixinSchemaDeclare
 {
     public function schema()
     {
         $kernel = kernel();
-        $this->column('comment')
+        $this->column('remark')
             ->text()
             ->label( _('備註') )
             ->renderAs( 'TextareaInput', [ 'rows' => 3, 'cols' => 60 ])
