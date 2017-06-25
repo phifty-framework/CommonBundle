523b846619c37f629110ba1a2b14556079cc115b
c9s <yoanlin93@gmail.com>
2017-06-25 19:02:13 +0800

    Clean up boot method

--- CommonBundle.php
+++ CommonBundle.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace CommonBundle;
+
 use Phifty\Bundle;
 
 class CommonBundle extends Bundle
@@ -8,41 +10,8 @@ class CommonBundle extends Bundle
 
     public function defaultConfig() { return array(); }
 
-    public function init() 
+    public function boot() 
     {
-        /*
-        $this->expandRoute( '/bs/product_resource', '\\Product\\ProductResourceCRUDHandler' );
-        $this->expandRoute( '/bs/product_image' ,   '\\Product\\ProductImageCRUDHandler' );
-        $this->expandRoute( '/bs/product_file' ,    '\\Product\\ProductFileCRUDHandler' );
 
-        if ( $this->config('with_agency_products') ) {
-            $this->expandRoute( '/bs/agency_product', '\\Product\\AgencyProductCRUD' );
-        }
-        if ( $this->config('with_types') ) {
-            $this->expandRoute( '/bs/product_type', '\\Product\\ProductTypeCRUDHandler' );
-        }
-
-        $this->route( '/bs/product/spec_panel', 'SpecPanelController');
-        $this->route( '/bs/product/api/delete_spec/{schemaId}' , 'SpecDataController:deleteSchemaAndData' );
-
-        // save/add spec data (item)
-        // $this->route( '/bs/product/api/save_spec_data', 'SpecDataController:saveSpecData');
-
-        $this->addRecordAction( 'Category' );
-        $this->addRecordAction( 'Product' , array(array('prefix' => 'BulkDelete')) );
-        $this->addRecordAction( 'ProductType' );
-        $this->addRecordAction( 'Feature' , array(array('prefix' => 'Delete')) );
-        $this->addRecordAction( 'FeatureRel' , array(
-            array('prefix' => 'Create'),
-            array('prefix' => 'Update'),
-            array('prefix' => 'Delete')
-        ) );
-        $this->addRecordAction( 'Resource' , array(
-            array('prefix' => 'Create'),
-            array('prefix' => 'Update'),
-            array('prefix' => 'Delete')
-        ) );
-        */
     }
-
 }
